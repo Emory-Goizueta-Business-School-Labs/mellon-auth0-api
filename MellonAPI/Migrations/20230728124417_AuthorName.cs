59fec97a4752b8c85083c74db0643811cd44c745
Brent Lineberry <brent.lineberry@emory.edu>
2023-07-28 08:59:14 -0400

    Authors endpoint

---
+++ MellonAPI/Migrations/20230728124417_AuthorName.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MellonAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AuthorName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Authors",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Authors");
+        }
+    }
+}
